c9eca48f248248d4f0df6e1156a29703a9609080
unknown <m_k_abdelkader@hotmail.com>
2020-12-16 14:51:49 -0400

    Update url

--- utils/urls.js
+++ utils/urls.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
-
-
-/*
-* Given an image return th Url
-* Works for local and deployed strapis
-* @parms{any} image
-*/
-export const fromImageToUrl = (image) => {
-	if(!image) {
-		return "/vercel2.svg"
-	}
-
-	if(image.url.indexOf("/") === 0) {
-		return `${API_URL}${image.url}`
-	}
-
-	return image.url
-=======
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
 
 
@@ -36,5 +16,4 @@ export const fromImageToUrl = (image) => {
 	}
 
 	return image.url
->>>>>>> 64f826a7b243ceff2fd1e3dba4b20d5313e57234
 }
